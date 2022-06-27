66ea0c1e97a5df850a65f4d59863b7ac8aa09d38
daorejuela1 <daorejuela1@outlook.com>
2022-06-26 19:31:53 -0500

    Added task 1 & 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Holberton example for doctstring with str or Text type
+"""
+
+def concat(a: str, b: str) -> str:
+    """
+    Concatenates two strings.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Holberton example for doctstring with str or Text type
+"""
+
+def floor(x: float) -> int:
+    """
+    Returns the floor of a float.
+    """
+    return int(x)
