6965bf2aeb6785848df04e30cfe32aa9ddf203b8
daorejuela1 <daorejuela1@outlook.com>
2022-06-26 20:38:36 -0500

    Added fith and sixth task

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+Creates a sum list of all the elements in the list
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums all the elements in the list
+    """
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import Union, List
+"""
+Sum mixed list of elements
+"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Sums all the elements in the list
+    """
+    return sum(mxd_lst)
