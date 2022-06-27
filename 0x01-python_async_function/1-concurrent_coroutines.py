0fd0b14d65974ba411c68c2300fc4d0ec0000983
daorejuela1 <daorejuela1@outlook.com>
2022-06-27 18:38:53 -0500

    Added second task

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Concurrent coroutines using async wait and asyncio.gather
+"""
+from asyncio import gather
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Wait for a random amount of time
+    """
+    gather_list = await gather(*[wait_random(max_delay) for _ in range(n)])
+    return sorted(gather_list)
