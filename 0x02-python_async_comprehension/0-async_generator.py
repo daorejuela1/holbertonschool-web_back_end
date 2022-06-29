73dbee475decc54eb39ae1e138110a2534a37dce
daorejuela1 <daorejuela1@outlook.com>
2022-06-28 22:18:30 -0500

    Fixed async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,15 @@
 Async random number generator
 """
 from random import uniform
+from asyncio import sleep
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """
     Async generator
     """
     for _ in range(10):
-        yield uniform(0, 10)
+        random_number: float = uniform(0, 10)
+        await sleep(1)
+        yield random_number
