29eed14c2bbcb8033da44e67de249dcf12504dcc
daorejuela1 <daorejuela1@outlook.com>
2022-06-28 23:28:49 -0500

    Fixed task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,14 +2,12 @@
 """
 Prints the data from the async generator
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Prints yielded values from the async generator
     """
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return(result)
+    return [i async for i in async_generator()]
