c6c4fdc0461dc74f61d02e66e70755f0440ef2b1
daorejuela1 <daorejuela1@outlook.com>
2022-06-28 23:33:54 -0500

    Fixed last task

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 Prints the data from the async generator
@@ -8,12 +7,12 @@ from asyncio import gather
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Prints yielded values from the async generator
     """
     start = time()
     number_of_executions = 4
-    gather(*[async_comprehension() for _ in range(number_of_executions)])
+    await gather(*[async_comprehension() for _ in range(number_of_executions)])
     end = time()
     return(end - start)
